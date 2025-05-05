b77fa00bd19fc4c50463937033fbc25aadeb9f61
Valerii Dychanskyi <dichansky@gmail.com>
2025-05-05 14:27:31 +0200

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,4 @@
--- basic settings --
-vim.opt.number = true
+-- basic settings -- vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
@@ -9,13 +8,22 @@ vim.opt.tabstop = 2
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- disable highlight when done searching
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- auto-install lazy.nvim --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
@@ -30,77 +38,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- plugins --
 require("lazy").setup({
-	{
-		"nvim-tree/nvim-tree.lua",
-		config = function()
-			require("nvim-tree").setup({})
-			vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "auto",
-					component_separators = "", -- remove symbols between components
-					section_separators = "", -- no funky arrows
-					globalstatus = true, -- one statusline for the whole screen
-					disabled_filetypes = { "NvimTree" },
-				},
-				sections = {
-					lualine_a = {}, -- remove mode (NORMAL/INSERT)
-					lualine_b = { "branch" }, -- only show Git branch
-					lualine_c = {}, -- no filename/path
-					lualine_x = {}, -- no encoding or filetype
-					lualine_y = {}, -- no progress
-					lualine_z = {}, -- no cursor position
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = {},
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = {},
-				},
-			})
-		end,
-	},
-
-	-- icons
-	{
-		"nvim-tree/nvim-web-devicons",
-		config = function()
-			require("nvim-web-devicons").setup({ default = true })
-
-			require("nvim-web-devicons").set_icon({
-				go = {
-					icon = "",
-					color = "#00ADD8",
-					name = "Go",
-				},
-				py = {
-					icon = "",
-					color = "#6A8EAE",
-					cterm_color = "74",
-					name = "Py",
-				},
-			})
-		end,
-	},
-
-	-- vscode ctrl + P
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup()
-			vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files, { desc = "Find files" })
-		end,
-	},
-
-	-- tree
+	-- file explorer
 	{
 		"nvim-tree/nvim-tree.lua",
 		config = function()
@@ -120,8 +58,7 @@ require("lazy").setup({
 					},
 				},
 			})
-
-			-- Remap Ctrl+B to toggle file explorer
+			vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 			vim.keymap.set(
 				"n",
 				"<C-b>",
@@ -130,50 +67,67 @@ require("lazy").setup({
 			)
 		end,
 	},
+
+	-- statusline
 	{
-		"neovim/nvim-lspconfig",
+		"nvim-lualine/lualine.nvim",
 		config = function()
-			require("lspconfig").lua_ls.setup({})
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+					component_separators = "",
+					section_separators = "",
+					globalstatus = true,
+					disabled_filetypes = { "NvimTree" },
+				},
+				sections = {
+					lualine_a = {},
+					lualine_b = { "branch" },
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+			})
 		end,
 	},
+
+	-- icons
 	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
-		},
+		"nvim-tree/nvim-web-devicons",
 		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+			require("nvim-web-devicons").setup({ default = true })
+			require("nvim-web-devicons").set_icon({
+				go = { icon = "", color = "#00ADD8", name = "Go" },
+				["go.mod"] = { icon = "", color = "#FF79C6", name = "Mod" },
+				["go.sum"] = { icon = "", color = "#FF79C6", name = "Sum" },
+				py = { icon = "", color = "#6A8EAE", cterm_color = "74", name = "Py" },
+			})
+		end,
+	},
+
+	-- telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup()
+			vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<C-f>", function()
+				require("telescope.builtin").live_grep({
+					additional_args = function()
+						return { "--smart-case" }
 					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-				},
-			})
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					go = { "gofmt", "goimports" },
-					lua = { "stylua" },
-					python = { "black", "isort", "ruff_format" },
-				},
-				format_on_save = {
-					lsp_fallback = true,
-					timeout_ms = 500,
-				},
-			})
+				})
+			end, { desc = "Telescope: Live grep (smart-case)" })
 		end,
 	},
 
@@ -181,7 +135,6 @@ require("lazy").setup({
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
-		config = true,
 		config = function()
 			require("gruvbox").setup({
 				terminal_colors = true,
@@ -202,14 +155,114 @@ require("lazy").setup({
 				invert_intend_guides = false,
 				inverse = true,
 				contrast = "hard",
-				palette_overrides = {},
-				overrides = {},
 				dim_inactive = false,
 				transparent_mode = false,
 			})
-
 			vim.o.background = "dark"
 			vim.cmd("colorscheme gruvbox")
+		end,
+	},
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			local function on_attach(_, bufnr)
+				local map = function(mode, lhs, rhs, desc)
+					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+				end
+
+				map("n", "gd", vim.lsp.buf.definition, "[G]o to [D]efinition")
+				map("n", "gr", vim.lsp.buf.references, "[G]o to [R]eferences")
+				map("n", "gi", vim.lsp.buf.implementation, "[G]o to [I]mplementation")
+				map("n", "K", vim.lsp.buf.hover, "Hover documentation")
+				map("n", "<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame symbol")
+				map("n", "<F2>", vim.lsp.buf.rename, "LSP Rename (F2)")
+			end
+
+			lspconfig.gopls.setup({
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+				on_attach = on_attach,
+				settings = {
+					gopls = {
+						analyses = {
+							unusedparams = true,
+							shadow = true,
+						},
+						staticcheck = true,
+					},
+				},
+			})
+
+			lspconfig.pyright.setup({
+				on_attach = on_attach,
+				settings = {
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							useLibraryCodeForTypes = true,
+							diagnosticMode = "workspace",
+						},
+					},
+				},
+			})
+
+			lspconfig.clangd.setup({
+				cmd = { "clangd" },
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+				root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
+				on_attach = on_attach,
+			})
+
+			lspconfig.lua_ls.setup({})
+		end,
+	},
+
+	-- completion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = { { name = "nvim_lsp" } },
+			})
+		end,
+	},
+
+	-- formatter
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					go = { "gofmt", "goimports" },
+					lua = { "stylua" },
+					python = { "black", "isort", "ruff_format" },
+					cpp = { "clang_format" },
+					c = { "clang_format" },
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					timeout_ms = 500,
+				},
+			})
 		end,
 	},
 
@@ -218,62 +271,97 @@ require("lazy").setup({
 		"github/copilot.vim",
 		event = "InsertEnter",
 	},
+
+	-- bufferline (vscode tabs)
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("bufferline").setup({})
+			vim.opt.termguicolors = true
+		end,
+	},
+
+	-- gitlense
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "▎" },
+					change = { text = "▎" },
+					delete = { text = "▎" },
+					topdelete = { text = "▎" },
+					changedelete = { text = "▎" },
+				},
+				current_line_blame = true, -- show inline Git blame like GitLens
+				current_line_blame_opts = {
+					delay = 300,
+					virt_text_pos = "eol",
+				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+					local function map(mode, l, r, desc)
+						vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+					end
+
+					-- Git hunk navigation
+					map("n", "]g", gs.next_hunk, "Next Git hunk")
+					map("n", "[g", gs.prev_hunk, "Prev Git hunk")
+
+					-- Git actions
+					map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+					map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+					map("n", "<leader>hb", gs.blame_line, "Blame line (full)")
+				end,
+			})
+		end,
+	},
+
+	-- neogit
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional, but adds great diff UI
+		},
+		config = function()
+			require("neogit").setup({
+				kind = "split", -- or "vsplit" or "tab"
+				signs = {
+					section = { "", "" },
+					item = { "", "" },
+				},
+				integrations = {
+					diffview = true,
+				},
+			})
+		end,
+	},
 })
 
--- Accept copilot suggestions son F1
+vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Neogit" })
+
+-- Copilot: Accept suggestion on F1
 vim.g.copilot_no_tab_map = true
-vim.keymap.set("i", "<F1>", 'copilot#Accept("<CR>")', {
+vim.api.nvim_set_keymap("i", "<F1>", 'copilot#Accept("")', {
 	expr = true,
 	silent = true,
-	script = true,
-	desc = "Copilot: Accept suggestion",
+	replace_keycodes = false,
 })
 
--- vscode ctrl + shift + F
-vim.keymap.set("n", "<C-f>", require("telescope.builtin").live_grep, { desc = "Live grep" })
+-- Buffer switching
+vim.keymap.set("n", "<C-]>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<C-h>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
 
--- LSP --
-local lspconfig = require("lspconfig")
+-- Buffer close
+vim.keymap.set("n", "<C-w>", ":bp | bd! #<CR>", { desc = "Close buffer" })
 
-local function on_attach(_, bufnr)
-	local map = function(mode, lhs, rhs, desc)
-		vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-	end
+-- Run `make test`
+vim.keymap.set("n", "<leader>mt", function()
+	vim.cmd("belowright split | terminal make test")
+end, { desc = "Make: test" })
 
-	map("n", "gd", vim.lsp.buf.definition, "[G]o to [D]efinition")
-	map("n", "gr", vim.lsp.buf.references, "[G]o to [R]eferences")
-	map("n", "gi", vim.lsp.buf.implementation, "[G]o to [I]mplementation")
-	map("n", "K", vim.lsp.buf.hover, "Hover documentation")
-	map("n", "<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame symbol")
-end
-
--- go
-lspconfig.gopls.setup({
-	cmd = { "gopls" },
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
-	root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-	on_attach = on_attach,
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-				shadow = true,
-			},
-			staticcheck = true,
-		},
-	},
-})
-
--- python
-lspconfig.pyright.setup({
-	on_attach = on_attach,
-	settings = {
-		python = {
-			analysis = {
-				autoSearchPaths = true,
-				useLibraryCodeForTypes = true,
-				diagnosticMode = "workspace",
-			},
-		},
-	},
-})
+-- clear search results
+vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
